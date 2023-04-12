cc7b95c87195b67e16d12224b985e54010dcf557
Lucien Gabet <lgabet@z4r9p5.42lyon.fr>
2023-04-12 13:06:42 +0200

    add mlx and compiled it

---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 12:56:42 by lgabet            #+#    #+#             */
+/*   Updated: 2023/04/12 13:03:50 by lgabet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx_linux/mlx.h"
+
+int main()
+{
+    void *mlx = mlx_init();
+    void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window - Create Image");
+
+    void *image = mlx_new_image(mlx, 640, 360);
+    
+    // The following code goes here.
+
+    mlx_loop(mlx);
+}
