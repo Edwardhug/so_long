44738dd3d2931ea1983739ae50b3ad5a7da84804
Lucien Gabet <lgabet@z4r9p5.42lyon.fr>
2023-04-12 14:53:30 +0200

    prog can display one sprite

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:56:42 by lgabet            #+#    #+#             */
-/*   Updated: 2023/04/12 13:30:37 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/04/12 14:52:26 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 
 int main()
 {
+	int height;
+	int width;
+	
+	height = 0;
+	width = 0;
 	void *mlx = mlx_init();
-	void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window - Create Image");
+	void *win = mlx_new_window(mlx, 640, 360, "Window");
 
 	void *image = mlx_new_image(mlx, 640, 360);
-	
+	void *dino = mlx_xpm_file_to_image(mlx, "sprite/knight.xpm", &width, &height);
+	if (dino == NULL)
+	{
+		ft_printf("wd");
+		exit(EXIT_FAILURE);
+	}
+	mlx_put_image_to_window(mlx, win, dino, 40, 40);
 	// The following code goes here.
 
 	mlx_loop(mlx);
