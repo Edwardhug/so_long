ccc7d350b90a35824718a75df340976d3cf47786
root <root@DESKTOP-7CO9QQK>
2023-05-12 17:46:20 +0200

    parsing normed

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 15:34:43 by lgabet            #+#    #+#             */
-/*   Updated: 2023/05/12 17:42:57 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/05/12 17:45:42 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	**ft_while_parsing(int number_of_line, char **map, int fd)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < number_of_line)
@@ -39,7 +39,10 @@ char	**ft_parsing(int ac, char **av)
 	char	**map;
 
 	if (ft_have_error_with_param(ac, av) == 1)
+	{
+		ft_printf("Error\nBad number of parameters\n");
 		return (NULL);
+	}
 	number_of_line = ft_count_line(av);
 	map = malloc(sizeof(char *) * (number_of_line + 1));
 	if (!map)
@@ -52,31 +55,20 @@ char	**ft_parsing(int ac, char **av)
 
 int	ft_have_error_with_param(int ac, char **av)
 {
-	size_t 	size;
+	size_t	size;
 	char	format[5];
 	int		i;
 
 	size = ft_strlen(av[1]);
 	ft_strlcpy(format, ".ber", 5);
 	i = 0;
-	if (ac != 2)
-	{
-		ft_printf("Error\nBad number of parameters\n");
+	if (ac != 2 || size < 4)
 		return (1);
-	}
-	if (size < 4)
-	{
-		ft_printf("Error\nBad map format\n");
-		return (1);
-	}
 	size = size - 4;
 	while (format[i])
 	{
 		if (format[i] != av[1][size + i])
-		{
-			ft_printf("Error\nBad map format\n");
 			return (1);
-		}
 		i++;
 	}
 	return (0);
@@ -86,7 +78,7 @@ int	ft_count_line(char **av)
 {
 	int		fd;
 	char	*line;
-	int 	i;
+	int		i;
 
 	fd = open(av[1], O_RDONLY);
 	i = 0;
